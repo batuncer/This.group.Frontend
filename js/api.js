8124ff129ce34fbed4c7cec988b0e35b45bf0ebc
allanstocco <allanstocco@hotmail.com>
2022-06-27 16:06:26 +0100

    rendering posts

--- js/api.js
+++ js/api.js
@@ -2,8 +2,33 @@ const renderPosts = () => {
 
     fetch('https://community-blog-server.herokuapp.com/api/blog')
         .then(response => response.json())
-        .then(res => console.log(res))
+        .then(res => res.forEach(data => {
+            const renderPost = document.querySelector('#render-posts');
+
+            renderPost.innerHTML += `
+            
+            <div>
+                <div>
+                    <h3>${data.title}</h3>
+                    <span>${data.date}</span>
+                    <p>${data.body}</p>
+                    <iframe src="${data.gif}" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                </div>
+                <div>
+                   ${data.comments}
+                </div>
+            </div>
+
+
+            `
+        }))
+
 
 }
 
-renderPosts()
+
+
+
+exports = {
+    renderPosts
+} 
