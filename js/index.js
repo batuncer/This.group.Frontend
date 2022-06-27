a070dc88a8b7479232f0a02e4f7d549c949ba6e8
allanstocco <allanstocco@hotmail.com>
2022-06-27 23:41:02 +0100

    giphy selector

--- js/index.js
+++ js/index.js
@@ -1,10 +1,45 @@
 const form = document.getElementById('form')
-const getGifList = document.querySelector('#gif_list')
+const listAllGifs = document.querySelector('#render_gifs_list');
 
-getGifList.addEventListener('click', (e) => {
-    console.log(e)
+const submitGifSearch = document.querySelector('#submit_search_giphy');
+const apiKey = 'rQYB70wWlBOocUdHVWOssNcEL7BVMjGy'
+
+
+submitGifSearch.addEventListener('click', (e) => {
+
+    const searchGifInput = document.querySelector('#search_input').value;
+
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${searchGifInput}&api_key=${apiKey}&&limit=12`)
+        .then(res => res.json())
+        .then(response => {
+            response.data.forEach(data => {
+                listAllGifs.innerHTML += `
+        
+                <input class="giphy-img" type="image" src="https://media0.giphy.com/media/${data.id}/giphy.gif?cid=ecf05e47lmco04g3zlrnwqck76d72v0t69ag28j0g0s1vhls&amp;rid=giphy.gif&amp;ct=g" width="120" height="120">
+                `
+                console.log(data)
+            }
+            )
+        })
 })
 
 renderPosts()
 form.addEventListener('submit', submitPost)
+
+
+// const getGiphy = async () => {
+
+//     const listAllGifs = document.querySelector('#render_gifs_list');
+//     const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=smile&api_key=${apiKey}&limit=20`)
+//     const giphyData = await response.json()
+//     giphyData.data.forEach(data => {
+//         listAllGifs.innerHTML += `
+
+//         <img class="giphy-img" src="https://media0.giphy.com/media/${data.id}/giphy.gif?cid=ecf05e47lmco04g3zlrnwqck76d72v0t69ag28j0g0s1vhls&amp;rid=giphy.gif&amp;ct=g" width="120" height="120">
+//         `
+
+//         console.log(data)
+
+//     })
+// }
 
